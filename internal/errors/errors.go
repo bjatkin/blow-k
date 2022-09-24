028f6f3962fea32bb08f70f92524d964c563f7ac
Brandon Atkinson <opensource.atkin@gmail.com>
2022-09-23 22:06:56 -0600

    feat: add New and Wrap to the errors package

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -18,3 +18,13 @@ const (
 
 // base error template
 var Base = bear.NewTemplate(bear.FmtPrettyPrint(true), bear.FmtNoID(true))
+
+// New returns a base new error
+func New(opts ...bear.ErrOption) *bear.Error {
+	return Base.New(opts...)
+}
+
+// Wrap returns a base wrapped error
+func Wrap(err error, opts ...bear.ErrOption) *bear.Error {
+	return Base.Wrap(err, opts...)
+}
